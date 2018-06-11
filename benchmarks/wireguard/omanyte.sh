e8bda452669e80accd67f98dddb4a1fe5ac0585d
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-06-11 17:43:55 +0200

    Update setup scripts

--- benchmarks/wireguard/omanyte.sh
+++ benchmarks/wireguard/omanyte.sh
@@ -1,19 +1,25 @@
-!/bin/bash
+#!/bin/bash
 
 set -e
 set -x
 
 apt-get update
-apt-get install -y linux-tools-4.4.0-109-generic
-apt-get install -y software-properties-common
-add-apt-repository ppa:wireguard/wireguard
+apt-get install -y linux-tools-`uname -r`
+apt-get install -y software-properties-common ifstat iftop git build-essential cmake linux-headers-`uname -r` lshw libnuma-dev htop fish
+add-apt-repository -y ppa:wireguard/wireguard
 apt-get update
 apt-get install -y wireguard
+
+ip addr flush dev ens1f0
+ip addr flush dev enp2s0f0
 
 ip a add 10.0.1.2/24 dev ens1f0
 ip a add 10.0.0.2/24 dev enp2s0f0
 ip l set up dev ens1f0
 ip l set up dev enp2s0f0
+ip l set mtu 1600 dev ens1f0
+ip l set mtu 1600 dev enp2s0f0
+ip l set mtu 1520 dev wg0 # https://lists.zx2c4.com/pipermail/wireguard/2017-December/002201.html
 sysctl -w net.ipv4.ip_forward=1
 
 ip link add dev wg0 type wireguard
@@ -33,3 +39,7 @@ EOF
 wg setconf wg0 /tmp/wg.conf
 ip l set up dev wg0
 ip r add 10.0.2.0/24 dev wg0
+
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
+echo run "ifstat -i enp2s0f0,ens1f0,wg0"
--- benchmarks/wireguard/omastar.sh
+++ benchmarks/wireguard/omastar.sh
@@ -1,24 +1,32 @@
-!/bin/bash
+#!/bin/bash
 
 set -e
 set -x
 
 apt-get update
-apt-get install -y linux-tools-4.4.0-109-generic
-apt-get install -y software-properties-common
-add-apt-repository ppa:wireguard/wireguard
+apt-get install -y linux-tools-`uname -r`
+apt-get install -y software-properties-common ifstat iftop git build-essential cmake linux-headers-`uname -r` lshw libnuma-dev htop fish
+add-apt-repository -y ppa:wireguard/wireguard
 apt-get update
 apt-get install -y wireguard
 
+ip addr flush dev ens3f1
+ip addr flush dev ens4f0
+
+
+# Receiving side
+
 ip a add 10.0.2.1/24 dev ens3f1
 ip a add 10.0.1.1/24 dev ens4f0
+ip l set mtu 1600 dev ens3f1
+ip l set mtu 1600 dev ens4f0
+ip l set mtu 1520 dev wg0 # https://lists.zx2c4.com/pipermail/wireguard/2017-December/002201.html
 ip l set up dev ens3f1
 ip l set up dev ens4f0
 
 ip link add dev wg0 type wireguard
 ip a add 192.168.0.1/24 dev wg0
 sysctl -w net.ipv4.ip_forward=1
-
 
 cat << EOF > /tmp/wg.conf
 [Interface]
@@ -36,3 +44,18 @@ ip l set up dev wg0
 ip r add 10.0.0.0/24 dev wg0
 
 arp -s 10.0.2.2 3C:FD:FE:9E:D7:41
+
+
+# Sending side
+
+git clone https://github.com/libmoon/libmoon.git
+cd libmoon
+./build.sh
+./setup-hugetlbfs.sh
+
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
+echo run "./build/libmoon ../pktgen.lua 0 5 --rate=100"
+echo "run \"perf stat -d -a -- sleep 60\""
+echo run "ifstat"
+echo run "iftop -pnNt"
