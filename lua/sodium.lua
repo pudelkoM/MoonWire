ffd2ed7c77f0e6d48ed1c1e4607a465864806cd0
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-06-24 13:49:01 +0200

    Export more helper functions

--- lua/sodium.lua
+++ lua/sodium.lua
@@ -7,6 +7,16 @@ local module = {}
 ffi.cdef[[
     int sodium_init(void);
 
+    void randombytes_buf(void * const buf, const size_t size);
+
+    void sodium_increment(unsigned char *n, const size_t nlen);
+
+    size_t crypto_aead_chacha20poly1305_ietf_keybytes(void);
+
+    size_t crypto_aead_chacha20poly1305_ietf_npubbytes(void);
+
+    size_t crypto_aead_chacha20poly1305_ietf_abytes(void);
+
     int crypto_aead_chacha20poly1305_ietf_encrypt(unsigned char *c,
                                               unsigned long long *clen,
                                               const unsigned char *m,
@@ -32,6 +42,26 @@ function module.sodium_init()
     return sodiumlib.sodium_init()
 end
 
+function module.randombytes_buf(buf, size)
+    sodiumlib.randombytes_buf(buf, size);
+end
+
+function module.sodium_increment(n, nlen)
+    sodiumlib.sodium_increment(n, nlen)
+end
+
+function module.crypto_aead_chacha20poly1305_ietf_keybytes()
+    return sodiumlib.crypto_aead_chacha20poly1305_ietf_keybytes()
+end
+
+function module.crypto_aead_chacha20poly1305_ietf_npubbytes()
+    return sodiumlib.crypto_aead_chacha20poly1305_ietf_npubbytes()
+end
+
+function module.crypto_aead_chacha20poly1305_ietf_abytes()
+    return sodiumlib.crypto_aead_chacha20poly1305_ietf_abytes()
+end
+
 function module.crypto_aead_chacha20poly1305_ietf_encrypt(c, clen, m, mlen, ad, adlen, nsec, npub, k)
     return sodiumlib.crypto_aead_chacha20poly1305_ietf_encrypt(c, clen, m, mlen, ad, adlen, nsec, npub, k)
 end
@@ -39,5 +69,9 @@ end
 function module.crypto_aead_chacha20poly1305_ietf_decrypt(m, mlen, nsec, c, clen, ad, adlen, npub, k)
     return sodiumlib.crypto_aead_chacha20poly1305_ietf_decrypt(m, mlen, nsec, c, clen, ad, adlen, npub, k)
 end
+
+module.crypto_aead_chacha20poly1305_IETF_KEYBYTES = sodiumlib.crypto_aead_chacha20poly1305_ietf_keybytes()
+module.crypto_aead_chacha20poly1305_IETF_NPUBBYTES = sodiumlib.crypto_aead_chacha20poly1305_ietf_npubbytes()
+module.crypto_aead_chacha20poly1305_IETF_ABYTES = sodiumlib.crypto_aead_chacha20poly1305_ietf_abytes()
 
 return module
