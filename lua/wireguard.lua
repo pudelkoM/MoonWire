59c1b8948277c105d665ac577c4ebc9950871b9f
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-06-24 13:50:49 +0200

    Encrypt & forward packets

--- lua/wireguard.lua
+++ lua/wireguard.lua
@@ -4,8 +4,9 @@ local lm = require "libmoon"
 local memory = require "memory"
 local log = require "log"
 local stats = require "stats"
+local dpdk_export = require "missing-dpdk-stuff"
 
-local msg = require "messages"
+-- local msg = require "messages"
 local sodium = require "sodium"
 
 local jit = require "jit"
@@ -34,9 +35,9 @@ function master(args)
         rssQueues = args.rxThreads
     }
     
-    device.waitForLinks()
+    -- device.waitForLinks()
 
-    stats.startStatsTask{devices = args.dev}
+    stats.startStatsTask{devices = {args.gateway, args.tunnel}}
 
     lm.startTask("slaveTaskEncrypt", args.gateway:getRxQueue(0), args.tunnel:getTxQueue(0))
 
@@ -45,7 +46,13 @@ function master(args)
 end
 
 local function handshake()
+    local txKey = ffi.new("uint8_t[?]", sodium.crypto_aead_chacha20poly1305_ietf_keybytes())
+    ffi.fill(txKey, sodium.crypto_aead_chacha20poly1305_ietf_keybytes(), 0xab)
 
+    local rxKey = ffi.new("uint8_t[?]", sodium.crypto_aead_chacha20poly1305_ietf_keybytes())
+    ffi.fill(rxKey, sodium.crypto_aead_chacha20poly1305_ietf_keybytes(), 0xef)
+
+    return txKey, rxKey
 end
 
 function slaveTaskEncrypt(gwDevQueue, tunDevQueue)
@@ -53,23 +60,79 @@ function slaveTaskEncrypt(gwDevQueue, tunDevQueue)
         log:error("Setting up libsodium")
         lm.stop()
     end
+    log:info("sodium init done")
+
+    local key, _ = handshake()
+    local nonce = ffi.new("uint8_t[?]", sodium.crypto_aead_chacha20poly1305_ietf_npubbytes())
+    print(nonce, sodium.crypto_aead_chacha20poly1305_ietf_npubbytes())
 
     local bufs = memory.bufArray()
-	while lm.running() do
-		local rx = gwDevQueue:tryRecv(bufs, 1000)
-		for i = 1, rx do
-            local pkt = bufs[i]:getEthernetPacket()
-            print(pkt)
+    while lm.running() do
+        local rx = gwDevQueue:tryRecv(bufs, 1000)
+        for i = 1, rx do
+            local buf = bufs[i]
             
-			-- -- swap MAC addresses
-			-- local pkt = bufs[i]:getEthernetPacket()
-			-- local tmp = pkt.eth:getDst()
-			-- pkt.eth:setDst(pkt.eth:getSrc())
-            -- pkt.eth:setSrc(tmp)
-            
+            local headroom, tailroom = dpdk_export.rte_pktmbuf_headroom_export(buf), dpdk_export.rte_pktmbuf_tailroom_export(buf)
 
-		end
-		tunDevQueue:sendN(bufs, rx)
-	end
+            -- size checks
+            if headroom < 16 then
+                log:error("pktmbuf headroom of " .. headroom .. "to small for 16 byte header")
+                lm.stop()
+                return
+            end
+
+            if tailroom < 8 then
+                log:error("pktmbuf tailroom of " .. tailroom .. "to small for " .. sodium.crypto_aead_chacha20poly1305_IETF_ABYTES .. " byte AEAD tag")
+                lm.stop()
+                return
+            end
+
+            -- save real IP packet sizes and pointer
+            local pkt = buf:getBytes() + 14
+            local len = buf:getSize() - 14
+
+            -- extend mbuf
+            if dpdk_export.rte_pktmbuf_append_export(buf, sodium.crypto_aead_chacha20poly1305_IETF_ABYTES) == nil then
+                log:error("Could not extend tailroom")
+                lm.stop()
+                return
+            end
+
+            local err = sodium.crypto_aead_chacha20poly1305_ietf_encrypt(
+                pkt, nil, -- cipertext (dst)
+                pkt, len, -- plaintext (src)
+                nil, 0,  -- addition data (unused)
+                nil, -- nsec (unused)
+                nonce, key
+            )
+            if err ~= 0 then
+                log:error("Error encrypting packet")
+                lm.stop()
+                return
+            end
+
+            -- Counter check
+            -- local cleartext = ffi.new("uint8_t[?]", len)
+            -- err = sodium.crypto_aead_chacha20poly1305_ietf_decrypt(
+            --     cleartext, nil, -- plaintext
+            --     nil, -- nsec (unused)
+            --     pkt, len + sodium.crypto_aead_chacha20poly1305_IETF_ABYTES, -- ciphertext
+            --     nil, 0, -- addition data (unused)
+            --     nonce, key
+            -- )
+            -- if err ~= 0 then
+            --     log:error("decrypt failed")
+            --     lm.stop()
+            --     return
+            -- end
+
+            sodium.sodium_increment(nonce, sodium.crypto_aead_chacha20poly1305_IETF_NPUBBYTES)
+
+            -- debug stats
+            --headroom, tailroom = dpdk_export.rte_pktmbuf_headroom_export(buf), dpdk_export.rte_pktmbuf_tailroom_export(buf)
+            --print(headroom, tailroom)
+        end
+        tunDevQueue:sendN(bufs, rx)
+    end
 
 end
