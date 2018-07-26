a49e8f7ac535df7aa8183753ea8721c830f13ea6
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-07-26 12:28:20 +0200

    Benchmark decryption directly
    Drop inplace option

--- benchmarks/libsodium/chacha20_poly1305_ietf.c
+++ benchmarks/libsodium/chacha20_poly1305_ietf.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <sodium.h>
 
-bool inplace = true;
+// TODO: also measure failure times
 
 static void handler(union sigval sv) {
     volatile sig_atomic_t *flag = sv.sival_ptr;
@@ -62,20 +62,15 @@ int main(void) {
     crypto_aead_chacha20poly1305_ietf_keygen(key);
     randombytes_buf(nonce, sizeof(nonce));
 
-    size_t block_sizes[] = {60, 128, 256, 512, 1280, 1514, 8000, 15 * 1024, 64 * 1024};
+    size_t block_sizes[] = {8, 16, 32, 64, 128, 256, 512, 1280, 1514, 2048, 8000, 15 * 1024, 64 * 1024};
 
     printf("# block_size, enc_operations_per_second, enc_Mbits_per_second, dec_operations_per_second, dec_Mbits_per_second\n");
 
     for (size_t i = 0; i < sizeof(block_sizes) / sizeof(*block_sizes); i++) {
         // Init
         const size_t bs = block_sizes[i];
-        if (inplace) {
-            message = malloc(bs + crypto_aead_chacha20poly1305_IETF_ABYTES);
-            ciphertext = message;
-        } else {
-            message = malloc(bs);
-            ciphertext = malloc(bs + crypto_aead_chacha20poly1305_IETF_ABYTES);
-        }
+        message = malloc(bs);
+        ciphertext = malloc(bs + crypto_aead_chacha20poly1305_IETF_ABYTES);
 
         // Bench encryption
         running = true;
@@ -115,48 +110,36 @@ int main(void) {
             exit(1);
         }
 
-        // Bench encryption + decryption
+        // Bench decryption
+        sodium_increment(nonce, sizeof(nonce));
+        crypto_aead_chacha20poly1305_ietf_encrypt(ciphertext, NULL, message, bs, NULL, 0, NULL, nonce, key);
         running = true;
         runs = 0;
         reset_timer(t);
         clock_gettime(CLOCK_MONOTONIC, &t1);
         while (running) {
-            sodium_increment(nonce, sizeof(nonce));
-            crypto_aead_chacha20poly1305_ietf_encrypt(ciphertext, NULL, message, bs, NULL, 0, NULL, nonce, key);
             crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);
-            
-            sodium_increment(nonce, sizeof(nonce));
-            crypto_aead_chacha20poly1305_ietf_encrypt(ciphertext, NULL, message, bs, NULL, 0, NULL, nonce, key);
             crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);
-            
-            sodium_increment(nonce, sizeof(nonce));
-            crypto_aead_chacha20poly1305_ietf_encrypt(ciphertext, NULL, message, bs, NULL, 0, NULL, nonce, key);
             crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);
-            
-            sodium_increment(nonce, sizeof(nonce));
-            crypto_aead_chacha20poly1305_ietf_encrypt(ciphertext, NULL, message, bs, NULL, 0, NULL, nonce, key);
             crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);
-            
-            runs += 4;
+            crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);
+            crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);
+            crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);
+            crypto_aead_chacha20poly1305_ietf_decrypt(message, NULL, NULL, ciphertext, bs + crypto_aead_chacha20poly1305_IETF_ABYTES, NULL, 0, nonce, key);            
+            runs += 8;
         }
         clock_gettime(CLOCK_MONOTONIC, &t2);
 
-        uint64_t elapsed_combined = (t2.tv_nsec + t2.tv_sec * 1e9) - (t1.tv_nsec + t1.tv_sec * 1e9);
-        double secs_combined = elapsed_combined / 1e9;
-        double ops_combined = runs / secs_combined;
-        double mbpps_combined = (bs * runs * 8) / (secs_combined * 1e6);
+        uint64_t elapsed_dec = (t2.tv_nsec + t2.tv_sec * 1e9) - (t1.tv_nsec + t1.tv_sec * 1e9);
+        double secs_dec = elapsed_dec / 1e9;
+        double ops_dec = runs / secs_dec;
+        double mbpps_dec = (bs * runs * 8) / (secs_dec * 1e6);
 
-        // Estimate decryption times
-        double ops_dec = 1 / ((1 / ops_combined) - (1 / ops_enc));
-        double mbpps_dec = 1 / ((1 / mbpps_combined) - (1 / mbpps_enc));
         printf("%zu, %.2lf, %.2lf, %.2lf, %.2lf\n", bs, ops_enc, mbpps_enc, ops_dec, mbpps_dec);
 
         // Cleanup
-        if (inplace) {
-            free(message);
-        } else {
-            free(message);
-            free(ciphertext);
-        }
+        free(message);
+        free(ciphertext);
     }
+    timer_delete(t);
 }
