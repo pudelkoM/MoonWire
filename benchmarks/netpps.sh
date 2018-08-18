5c88d4f6d2403a9a33ba0402ac49aa84c24b68a1
Maximilian Pudelko <maximilian.pudelko@tum.de>
2018-08-18 11:02:13 +0200

    make packet counter more script friendly

--- benchmarks/netpps.sh
+++ benchmarks/netpps.sh
@@ -17,14 +17,16 @@ INTERVAL="$2"  # update interval in seconds
 R1=`cat /sys/class/net/$1/statistics/rx_packets`
 T1=`cat /sys/class/net/$1/statistics/tx_packets`
 
+echo "# time, interface, tx_mpps, rx_mpps"
+
 while true
 do
         sleep $INTERVAL
         R2=`cat /sys/class/net/$1/statistics/rx_packets`
         T2=`cat /sys/class/net/$1/statistics/tx_packets`
-        TXPPS=`echo "scale=2; ($T2 - $T1) / ($INTERVAL * 1000000)" | bc`
-        RXPPS=`echo "scale=2; ($R2 - $R1) / ($INTERVAL * 1000000)" | bc`
-        echo "$(date -Iseconds) $1 TX $TXPPS Mpps, RX $RXPPS Mpps"
+        TXMPPS=`echo "scale=2; ($T2 - $T1) / ($INTERVAL * 1000000)" | bc`
+        RXMPPS=`echo "scale=2; ($R2 - $R1) / ($INTERVAL * 1000000)" | bc`
+        echo "$(date -Iseconds), $1, $TXMPPS, $RXMPPS"
         R1=$R2
         T1=$T2
 done
