44cbc7436060778037d295a08be2fcc985e7d386
Maximilian Pudelko <maximilian.pudelko@maibornwolff.de>
2018-09-20 14:05:56 +0200

    Reverse benchmark setup

--- benchmarks/wireguard/omanyte.sh
+++ benchmarks/wireguard/omanyte.sh
@@ -6,6 +6,8 @@ set -x
 IF1=enp2s0f0 # 68:05:CA:32:44:D8, 0000:02:00.0, dpdk
 IF2=ens1f0 # 68:05:CA:32:44:98, 0000:04:00.0
 
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
 apt-get update
 apt-get install -y linux-tools-`uname -r`
 apt-get install -y software-properties-common ifstat iftop git build-essential cmake linux-headers-`uname -r` lshw libnuma-dev htop fish
@@ -13,18 +15,15 @@ add-apt-repository -y ppa:wireguard/wireguard
 apt-get update
 apt-get install -y wireguard
 
+ip addr flush dev $IF1
 ip addr flush dev $IF2
-# ip addr flush dev ens4f0
 
 
 # Receiving side
 
-ip a add 10.0.1.1/24 dev $IF2
-# ip a add 10.0.2.1/24 dev ens3f1
+ip a add 10.1.0.2/24 dev $IF2
 ip l set mtu 1600 dev $IF2
-# ip l set mtu 1600 dev ens4f0
 ip l set up dev $IF2
-# ip l set up dev ens4f0
 
 ip link add dev wg0 type wireguard
 ip l set mtu 1520 dev wg0 # https://lists.zx2c4.com/pipermail/wireguard/2017-December/002201.html
@@ -38,15 +37,15 @@ ListenPort = 1234
 
 [Peer]
 PublicKey  = 66tH+Yi9hNpskN6aPVSiCB+suSIf4KBzv8evR1eg1Eg=
-EndPoint   = 10.0.1.2:1234
-AllowedIPs = 10.0.1.0,10.0.0.0/24,10.0.2.0/24
+EndPoint   = 10.1.0.1:1234
+AllowedIPs = 10.0.0.0/16,10.1.0.0/16,10.2.0.0/16
 EOF
 
 wg setconf wg0 /tmp/wg.conf
 ip l set up dev wg0
-ip r add 10.0.0.0/24 dev wg0
+# ip r add 10.0.0.0/24 dev wg0
 
-arp -s 10.0.2.2 3C:FD:FE:9E:D7:41
+# arp -s 10.0.2.2 3C:FD:FE:9E:D7:41
 
 
 # Sending side
@@ -55,10 +54,3 @@ arp -s 10.0.2.2 3C:FD:FE:9E:D7:41
 # cd libmoon
 # ./build.sh
 # ./setup-hugetlbfs.sh
-
-echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
-
-echo run "./build/libmoon ../pktgen.lua 0 5 --rate=100"
-echo "run \"perf stat -d -a -- sleep 60\""
-echo run "ifstat"
-echo run "iftop -pnNt"
--- benchmarks/wireguard/omastar.sh
+++ benchmarks/wireguard/omastar.sh
@@ -6,6 +6,8 @@ set -x
 IF1=ens3f0 # 3C:FD:FE:9E:D6:B8, 0000:03:00.0
 IF2=ens4f0 # 3C:FD:FE:9E:D7:40, 0000:83:00.0
 
+echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
 apt-get update
 apt-get install -y linux-tools-`uname -r`
 apt-get install -y software-properties-common ifstat iftop git build-essential cmake linux-headers-`uname -r` lshw libnuma-dev htop fish
@@ -16,8 +18,8 @@ apt-get install -y wireguard
 ip addr flush dev $IF1
 ip addr flush dev $IF2
 
-ip a add 10.0.0.2/24 dev $IF1
-ip a add 10.0.1.2/24 dev $IF2
+ip a add 10.0.0.1/16 dev $IF1
+ip a add 10.1.0.1/24 dev $IF2
 ip l set mtu 1600 dev $IF1
 ip l set mtu 1600 dev $IF2
 ip l set up dev $IF1
@@ -35,14 +37,13 @@ ListenPort = 1234
 
 [Peer]
 PublicKey  = fDd9tw3LEh8sp5KQup4Xs0y9K0SOU9mJ18haDDIixGM=
-EndPoint   = 10.0.1.1:1234
-AllowedIPs = 10.0.1.1,10.0.0.0/24,10.0.2.0/24,192.168.0.0/24
+EndPoint   = 10.1.0.2:1234
+AllowedIPs = 10.0.0.0/16,10.1.0.0/16,10.2.0.0/16
 EOF
 
 wg setconf wg0 /tmp/wg.conf
 ip l set up dev wg0
-ip r add 10.0.2.0/24 dev wg0
+ip r add 10.2.0.0/16 dev wg0
 
-echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
 
-echo run "ifstat -i enp2s0f0,ens1f0,wg0"
+# perf stat -a -A -x , --cpu=0,1,2 -dd -- sleep 30
