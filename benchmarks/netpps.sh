636799a51009668561c517390c809e64eb904031
Maximilian Pudelko <maximilian.pudelko@maibornwolff.de>
2018-09-20 12:49:04 +0200

    Fix packet counter script for native drivers

--- benchmarks/netpps.sh
+++ benchmarks/netpps.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
+set -e
 
-if [ -z "$2" ]; then
+if [ -z "$1" ]; then
         echo
-        echo usage: $0 [network-interface] [interval]
+        echo usage: $0 [network-interface] ...
         echo
-        echo e.g. $0 eth0 1
+        echo e.g. $0 eth0
         echo
         echo shows packets-per-second
         exit
 fi
 
-IF=$1
-INTERVAL="$2"  # update interval in seconds
+IFS="${@:1}"
 
-R1=`cat /sys/class/net/$1/statistics/rx_packets`
-T1=`cat /sys/class/net/$1/statistics/tx_packets`
+sleepUntilFullSecond() {
+        sleep "$(echo "scale=6; (1000000000-$(date +%N))/1000000000" | bc)s"
+}
 
-echo "# time, interface, tx_mpps, rx_mpps"
+declare -A ifs
+
+for if in "${@:1}";
+do
+        ifs["$if"_rx]=`cat /sys/class/net/"$if"/statistics/rx_packets`
+        ifs["$if"_tx]=`cat /sys/class/net/"$if"/statistics/tx_packets`
+done
+
+echo "# time, interface, rx_mpps, tx_mpps"
 
 while true
 do
-        sleep $INTERVAL
-        R2=`cat /sys/class/net/$1/statistics/rx_packets`
-        T2=`cat /sys/class/net/$1/statistics/tx_packets`
-        TXMPPS=`echo "scale=2; ($T2 - $T1) / ($INTERVAL * 1000000)" | bc`
-        RXMPPS=`echo "scale=2; ($R2 - $R1) / ($INTERVAL * 1000000)" | bc`
-        echo "$(date -Iseconds), $1, $TXMPPS, $RXMPPS"
-        R1=$R2
-        T1=$T2
+        sleepUntilFullSecond
+        for if in "${@:1}";
+        do
+                R1="${ifs["$if"_rx]}"
+                T1="${ifs["$if"_tx]}"
+                R2=`cat /sys/class/net/"$if"/statistics/rx_packets`
+                T2=`cat /sys/class/net/"$if"/statistics/tx_packets`
+                TXMPPS=`echo "scale=2; ($T2 - $T1) / 1000000" | bc`
+                RXMPPS=`echo "scale=2; ($R2 - $R1) / 1000000" | bc`
+                echo "$(date -Iseconds), $if, $RXMPPS, $TXMPPS"
+                ifs["$if"_rx]=$R2
+                ifs["$if"_tx]=$T2
+        done
 done
